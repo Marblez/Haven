4c77cfc201a3d27f78441d02f1638945ea60fc78
Marblez <MatthewWang2020@u.northwestern.edu>
2018-02-25 04:21:32 -0600

    Site Graphs

--- graph.js
+++ graph.js
@@ -1,7 +1,7 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
 google.charts.setOnLoadCallback(drawLineColors);
 
-function daily() {
+function emotions() {
       var data = new google.visualization.DataTable();
       data.addColumn('number', 'X');
       data.addColumn('number', 'Time');
@@ -25,7 +25,7 @@ function daily() {
       chart.draw(data, options);
     }
 
-    function weekly() {
+    function sentiment() {
           var data = new google.visualization.DataTable();
           data.addColumn('number', 'X');
           vAxis: { gridlines: { count: 100 } };
@@ -53,26 +53,4 @@ function daily() {
           chart.draw(data, options);
         }
 
-        function monthly() {
-              var data = new google.visualization.DataTable();
-              data.addColumn('number', 'X');
-              data.addColumn('number', 'Time');
-              data.addRows([
-                [0, 0],    [1, 100],   [2, 23],  [3, 17],   [4, 18],  [5, 9],
-                [6, 11],   [7, 27],  [8, 100],  [9, 40],  [10, 32], [11, 35],[12, 12], [13, 35], [14, 19], [15, 25], [16, 21], [17, 35],[18, 44],[19, 3],[20, 5],[21, 55],[22, 30],[23, 35],[24, 70],[25, 15],[26, 50],[27, 70],[28, 40],[29, 12],[30, 35],
-
-              ]);
-
-              var options = {
-                hAxis: {
-                  title: 'March 2018'
-                },
-                vAxis: {
-                  title: 'Minutes per Day'
-                },
-                colors: ['#a52714', '#097132']
-              };
-
-              var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-              chart.draw(data, options);
-            }
+     
